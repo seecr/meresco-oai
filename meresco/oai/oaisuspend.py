79c56846e0b78797924e30b60fdcf8db63a5acd6
jurjanpaul <jurjanpaul@83e7a582-4515-4195-8f39-222cbda6297e>
2010-10-06 13:56:31 +0000

    HM/JPM: fixed license
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-oai/workingsets/3.2.1-Edurep/version_3-oai-near-realtime-harvest@2848 83e7a582-4515-4195-8f39-222cbda6297e

--- meresco/oai/oaisuspend.py
+++ meresco/oai/oaisuspend.py
@@ -2,12 +2,8 @@
 #
 #    Meresco Oai are components to build Oai repositories, based on Meresco
 #    Core and Meresco Components.
-#    Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
-#    Copyright (C) 2007-2009 Stichting Kennisnet Ict op school.
+#    Copyright (C) 2010 Stichting Kennisnet Ict op school.
 #       http://www.kennisnetictopschool.nl
-#    Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
-#    Copyright (C) 2009 Tilburg University http://www.uvt.nl
-#    Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 #
 #    This file is part of Meresco Oai.
 #
