401031db956a3b8242b058119c80044bfb503a5f
tomvdsom <tomvdsom@83e7a582-4515-4195-8f39-222cbda6297e>
2012-01-30 12:20:10 +0000

    JPM/TS: testsetup fixed.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-oai/workingsets/4.0-beta2-Seecr/version_0-meresco4@4252 83e7a582-4515-4195-8f39-222cbda6297e

--- testsetup.sh
+++ testsetup.sh
@@ -2,19 +2,28 @@
 set -e
 mydir=$(cd $(dirname $0); pwd)
 
+fullPythonVersion=python2.6
+
+VERSION="x.y.z"
+
 rm -rf tmp build
-python2.5 setup.py install --root tmp
+${fullPythonVersion} setup.py install --root tmp
 
-cp meresco/__init__.py tmp/usr/lib/python2.5/site-packages/meresco
-export PYTHONPATH=`pwd`/tmp/usr/lib/python2.5/site-packages
 cp -r test tmp/test
-find tmp -type f -exec sed -e \
+find tmp -type f -exec sed -r -e \
     "/DO_NOT_DISTRIBUTE/d;
-    s,^binDir.*$,binDir='$mydir/tmp/usr/bin'," -i {} \;
+    s,^binDir.*$,binDir='$mydir/tmp/usr/local/bin',;
+    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/" -i {} \;
 
+cp meresco/__init__.py tmp/usr/local/lib/${fullPythonVersion}/dist-packages/meresco
+export PYTHONPATH=`pwd`/tmp/usr/local/lib/${fullPythonVersion}/dist-packages:${PYTHONPATH}
+
+set +o errexit
 (
 cd tmp/test
 ./alltests.sh
 )
+set -o errexit
+exit 123
 
 rm -rf tmp build
