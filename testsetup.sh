ed1a1071a0c21fc82e8ba4974e44e998fad6c2d5
Thijs Janssen <thijs@seecr.nl>
2013-05-10 16:50:57 +0200

    TJ: setup fixed to new style.

--- testsetup.sh
+++ testsetup.sh
@@ -1,63 +1,53 @@
 #!/bin/bash
 ## begin license ##
-# 
+#
 # "Meresco Oai" are components to build Oai repositories, based on
-# "Meresco Core" and "Meresco Components". 
-# 
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# "Meresco Core" and "Meresco Components".
+#
+# Copyright (C) 2012-2013 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Meresco Oai"
-# 
+#
 # "Meresco Oai" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Oai" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Oai"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-set -e
+set -o errexit
+rm -rf tmp build
 mydir=$(cd $(dirname $0); pwd)
+source /usr/share/seecr-test/functions
 
-fullPythonVersion=python2.6
+pyversions="2.6"
+if distro_is_debian_wheezy; then
+    pyversions="2.6 2.7"
+fi
 
 VERSION="x.y.z"
 
-rm -rf tmp build
-${fullPythonVersion} setup.py install --root tmp
-
-if [ -f /etc/debian_version ]; then
-    USR_DIR=`pwd`/tmp/usr/local
-    SITE_PACKAGE_DIR=${USR_DIR}/lib/${fullPythonVersion}/dist-packages
-else
-    USR_DIR=`pwd`/tmp/usr
-    SITE_PACKAGE_DIR=${USR_DIR}/lib/${fullPythonVersion}/site-packages
-fi
-
-cp meresco/__init__.py ${SITE_PACKAGE_DIR}/meresco
-export PYTHONPATH=${SITE_PACKAGE_DIR}:${PYTHONPATH}
+for pyversion in $pyversions; do
+    definePythonVars $pyversion
+    echo "###### $pyversion, $PYTHON"
+    ${PYTHON} setup.py install --root tmp
+done
+cp -r test tmp/test
+removeDoNotDistribute tmp
+find tmp -name '*.py' -exec sed -r -e "
+    s,^binDir.*$,binDir='${SEECRTEST_USR_BIN}',;
+    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
+    " -i '{}' \;
 
 cp -r test tmp/test
-
-find tmp -type f -exec sed -r -e \
-    "/DO_NOT_DISTRIBUTE/d;
-    s,^binDir.*$,binDir='${USR_DIR}/bin',;
-    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/" -i {} \;
-
-
-set +o errexit
-(
-cd tmp/test
-./alltests.sh
-)
-set -o errexit
-
+runtests "$@"
 rm -rf tmp build
