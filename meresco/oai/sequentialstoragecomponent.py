5df8d08b8b46cebe3ebfe185ab271af3078ddc53
Hendrik Moree <hendrik@seecr.nl>
2014-03-13 15:36:12 +0100

    EG/HM: stamp not indexed, only stored

--- meresco/oai/sequentialstoragecomponent.py
+++ meresco/oai/sequentialstoragecomponent.py
@@ -28,25 +28,25 @@ from meresco.oai import SequentialMultiStorage
 from meresco.core import asyncnoreturnvalue
 from cStringIO import StringIO
 from time import time
-from oaijazz import lazyImport
 from time import sleep
 
 
-from lucene import initVM, getVMEnv
+from lucene import initVM
 from os.path import join
 initVM()
 
-from java.lang import Long
 from java.io import File
-from org.apache.lucene.document import Document, StringField, Field, LongField
-from org.apache.lucene.search import IndexSearcher, TermQuery, BooleanQuery, NumericRangeQuery, MatchAllDocsQuery
-from org.apache.lucene.search import BooleanClause, TotalHitCountCollector, Sort, SortField
+from org.apache.lucene.document import Document, StringField, Field, LongField, FieldType
+from org.apache.lucene.search import IndexSearcher, TermQuery
 from org.apache.lucene.index import DirectoryReader, Term, IndexWriter, IndexWriterConfig
 from org.apache.lucene.store import FSDirectory
-from org.apache.lucene.document import NumericDocValuesField, StoredField
-from org.apache.lucene.index.sorter import SortingMergePolicy, NumericDocValuesSorter
-from org.apache.lucene.util import BytesRef, Version
+from org.apache.lucene.util import Version
 from org.apache.lucene.analysis.core import WhitespaceAnalyzer
+
+StampType = FieldType()
+StampType.setIndexed(False)
+StampType.setStored(True)
+StampType.setNumericType(FieldType.NumericType.LONG)
 
 def getLucene(path):
     directory = FSDirectory.open(File(path))
@@ -67,7 +67,7 @@ class Index(object):
     def __setitem__(self, key, value):
         doc = Document()
         doc.add(StringField("key", key, Field.Store.NO))
-        doc.add(LongField("value", long(value), Field.Store.YES))
+        doc.add(LongField("value", long(value), StampType))
         self._writer.updateDocument(Term("key", key), doc)
         self._latestModifications[key] = value
 
@@ -78,11 +78,9 @@ class Index(object):
         elif stamp is not None:
             return stamp
         if len(self._latestModifications) > 10000:
-            newreader = DirectoryReader.openIfChanged(self._reader, self._writer, True)
-            if newreader:
-                self._reader = newreader
-                self._searcher = IndexSearcher(newreader)
-                self._latestModifications.clear()
+            self._reader = DirectoryReader.openIfChanged(self._reader, self._writer, True)
+            self._searcher = IndexSearcher(self._reader)
+            self._latestModifications.clear()
         topDocs = self._searcher.search(TermQuery(Term("key", key)), 1)
         if topDocs.totalHits == 0:
             raise KeyError("Record deleted")
