4f21a011f48b6820852c38cb75efc23c651b9968
Thijs Janssen <thijs@seecr.nl>
2014-11-13 10:00:58 +0100

    TJ: create data with check of the expected data.

--- test/data/create_v7_data.py
+++ test/data/create_v7_data.py
@@ -26,13 +26,12 @@
 ## end license ##
 
 from meresco.oai import OaiJazz
+from os.path import isdir
+from shutil import rmtree
 
-def main():
-    if OaiJazz.version != '7':
-        print 'Please run with version 7 of OaiJazz'
-        exit(1)
-
-    j = OaiJazz('oai_conversion_v7_to_v8')
+def create(datapath):
+    isdir(datapath) and rmtree(datapath)
+    j = OaiJazz(datapath)
     j.addOaiRecord('oai:1', metadataFormats=[('oai_dc', 'schema', 'namespace')])
     j.addOaiRecord('oai:2', metadataFormats=[('oai_dc', 'schema', 'namespace')])
     j.addOaiRecord('oai:3', metadataFormats=[('oai_dc', 'schema', 'namespace')])
@@ -44,6 +43,19 @@ def main():
     j.addOaiRecord('oai:4', metadataFormats=[('oai_dc', 'schema', 'namespace')])
     list(j.delete('oai:2'))
     j.close()
+
+def main():
+    if OaiJazz.version != '7':
+        print 'Please run with version 7 of OaiJazz'
+        exit(1)
+    create('oai_conversion_v7_to_v8')
+    j = OaiJazz('oai_conversion_v7_to_v8')
+    result = j.oaiSelect(prefix='oai_dc')
+    records = list(result.records)
+    assert ['oai:1', 'oai:3', 'oai:5', 'oai:4', 'oai:2'] == [r.identifier for r in records]
+    assert [False, False, False, False, True] == [r.isDeleted for r in records]
+    j.close()
+
 
 if __name__ == '__main__':
     main()
