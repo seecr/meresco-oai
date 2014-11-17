2c275b178a594b569ab5132cac4e81fff0a6cc9e
Thijs Janssen <thijs@seecr.nl>
2014-11-17 16:09:41 +0100

    JPM/TJ: getData instead of getStream used by OaiProvenance

--- meresco/oai/oaiprovenance.py
+++ meresco/oai/oaiprovenance.py
@@ -1,35 +1,38 @@
 ## begin license ##
-# 
+#
 # "Meresco Oai" are components to build Oai repositories, based on
-# "Meresco Core" and "Meresco Components". 
-# 
+# "Meresco Core" and "Meresco Components".
+#
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
 # Copyright (C) 2010 Stichting Kennisnet http://www.kennisnet.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Netherlands Institute for Sound and Vision http://instituut.beeldengeluid.nl/
+#
 # This file is part of "Meresco Oai"
-# 
+#
 # "Meresco Oai" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Oai" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Oai"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from meresco.components import XmlCompose
+from lxml.etree import parse
+from StringIO import StringIO
 
 PROVENANCE_TEMPLATE = """<provenance xmlns="http://www.openarchives.org/OAI/2.0/provenance"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -37,10 +40,10 @@ PROVENANCE_TEMPLATE = """<provenance xmlns="http://www.openarchives.org/OAI/2.0/
                       http://www.openarchives.org/OAI/2.0/provenance.xsd">
 
 <originDescription harvestDate="%(harvestDate)s" altered="true">
-  <baseURL>%(baseURL)s</baseURL>
-  <identifier>%(identifier)s</identifier>
-  <datestamp>%(datestamp)s</datestamp>
-  <metadataNamespace>%(metadataNamespace)s</metadataNamespace>
+    <baseURL>%(baseURL)s</baseURL>
+    <identifier>%(identifier)s</identifier>
+    <datestamp>%(datestamp)s</datestamp>
+    <metadataNamespace>%(metadataNamespace)s</metadataNamespace>
 </originDescription>
 </provenance>
 """
@@ -58,4 +61,6 @@ class OaiProvenance(XmlCompose):
 
     def provenance(self, identifier):
         yield self.getRecord(identifier=identifier)
-        
+
+    def _getPart(self, identifier, partname):
+        return parse(StringIO(self.call.getData(identifier=identifier, name=partname)))
--- test/oaiprovenancetest.py
+++ test/oaiprovenancetest.py
@@ -1,39 +1,44 @@
 ## begin license ##
-# 
+#
 # "Meresco Oai" are components to build Oai repositories, based on
-# "Meresco Core" and "Meresco Components". 
-# 
+# "Meresco Core" and "Meresco Components".
+#
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# Copyright (C) 2012, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2014 Netherlands Institute for Sound and Vision http://instituut.beeldengeluid.nl/
+#
 # This file is part of "Meresco Oai"
-# 
+#
 # "Meresco Oai" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Oai" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Oai"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-from seecr.test import CallTrace, SeecrTestCase
+from seecr.test import SeecrTestCase, CallTrace
 from StringIO import StringIO
 
-from weightless.core import compose
+from weightless.core import asString, be
 from meresco.core import Observable
 from meresco.oai.oaiprovenance import OaiProvenance
+from meresco.oai import OaiPmh
+from meresco.xml.namespaces import xpathFirst
+from lxml.etree import XML
+from meresco.components.http.utils import CRLF
 
 
 class OaiProvenanceTest(SeecrTestCase):
@@ -53,7 +58,7 @@ class OaiProvenanceTest(SeecrTestCase):
         provenance.addObserver(observer)
 
         self.assertEquals(0, storage.timesCalled)
-        result = ''.join(list(compose(observable.any.provenance("recordId"))))
+        asString(observable.any.provenance("recordId"))
         self.assertEquals(2, storage.timesCalled)
 
     def testProvenance(self):
@@ -70,19 +75,68 @@ class OaiProvenanceTest(SeecrTestCase):
         observer = MockStorage()
         provenance.addObserver(observer)
 
-        result = ''.join(list(compose(observable.any.provenance("recordId"))))
-        self.assertEqualsWS(result, """<provenance xmlns="http://www.openarchives.org/OAI/2.0/provenance"
+        result = asString(observable.any.provenance("recordId"))
+        self.assertEqualsWS("""<provenance xmlns="http://www.openarchives.org/OAI/2.0/provenance"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/provenance
                       http://www.openarchives.org/OAI/2.0/provenance.xsd">
 
 <originDescription harvestDate="HARVESTDATE" altered="true">
   <baseURL>BASEURL</baseURL>
-  <identifier>IDENTIFIER</identifier>
+  <identifier>recordId</identifier>
   <datestamp>DATESTAMP</datestamp>
   <metadataNamespace>METADATANAMESPACE</metadataNamespace>
 </originDescription>
-</provenance>""")
+</provenance>""", result)
+
+    def testProvenanceFromOaiPmh(self):
+        def getRecord(identifier, *args, **kwargs):
+            record = CallTrace()
+            record.identifier = identifier
+            record.prefixes = set(['oai_dc'])
+            record.sets = set()
+            record.isDeleted = False
+            return record
+        oaijazz = CallTrace(methods={'getRecord': getRecord, 'getAllPrefixes': lambda: set(['oai_dc'])}, onlySpecifiedMethods=True)
+        observable = be((Observable(),
+            (OaiPmh(
+                repositoryName='example',
+                adminEmail='no@example.com'),
+                (oaijazz,),
+                (MockStorage(),),
+                (OaiProvenance(
+                        nsMap = {'oai_dc': "http://www.openarchives.org/OAI/2.0/"},
+                        baseURL = ('meta', '/meta/repository/baseurl/text()'),
+                        harvestDate = ('meta', '/meta/repository/harvestDate/text()'),
+                        metadataNamespace = ('meta', '/meta/repository/metadataNamespace/text()'),
+                        identifier = ('header','/oai_dc:header/oai_dc:identifier/text()'),
+                        datestamp = ('header', '/oai_dc:header/oai_dc:datestamp/text()')
+                    ),
+                    (MockStorage(),),
+                )
+            )
+        ))
+
+        result = asString(observable.all.handleRequest(
+                Method='GET',
+                arguments=dict(verb=['GetRecord'], identifier=["recordId"], metadataPrefix=['oai_dc']),
+                Headers=dict(Host='oaiserver.example.org'),
+                path='/oai',
+                port=1234,
+            ))
+        provenanceResult = xpathFirst(XML(result.split(CRLF*2)[-1]), '//oai:about/oaiprov:provenance')
+        self.assertXmlEquals("""<provenance xmlns="http://www.openarchives.org/OAI/2.0/provenance"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/provenance
+                      http://www.openarchives.org/OAI/2.0/provenance.xsd">
+
+<originDescription harvestDate="HARVESTDATE" altered="true">
+  <baseURL>BASEURL</baseURL>
+  <identifier>recordId</identifier>
+  <datestamp>DATESTAMP</datestamp>
+  <metadataNamespace>METADATANAMESPACE</metadataNamespace>
+</originDescription>
+</provenance>""", provenanceResult)
 
     def testNoOutputIfValueMissing(self):
         observable = Observable()
@@ -98,21 +152,24 @@ class OaiProvenanceTest(SeecrTestCase):
         observer = MockStorage()
         provenance.addObserver(observer)
 
-        result = ''.join(list(compose(observable.any.provenance("recordId"))))
+        result = asString(observable.any.provenance("recordId"))
         self.assertEquals('', result)
+
 
 class MockStorage(object):
     def __init__(self):
         self.timesCalled = 0
 
-    def getStream(self, ident, partname):
+    def getData(self, identifier, name):
         self.timesCalled += 1
-        if partname == 'meta':
-            return StringIO("<meta><repository><metadataNamespace>METADATANAMESPACE</metadataNamespace><baseurl>BASEURL</baseurl><harvestDate>HARVESTDATE</harvestDate></repository></meta>")
-        elif partname == 'header':
-            return StringIO("""<header xmlns="http://www.openarchives.org/OAI/2.0/">
-    <identifier>IDENTIFIER</identifier>
+        if name == 'meta':
+            return "<meta><repository><metadataNamespace>METADATANAMESPACE</metadataNamespace><baseurl>BASEURL</baseurl><harvestDate>HARVESTDATE</harvestDate></repository></meta>"
+        elif name == 'header':
+            return """<header xmlns="http://www.openarchives.org/OAI/2.0/">
+    <identifier>{0}</identifier>
     <datestamp>DATESTAMP</datestamp>
 
-  </header>""")
+  </header>""".format(identifier)
+        elif name == 'oai_dc':
+            return """<oai_dc/>"""
 
