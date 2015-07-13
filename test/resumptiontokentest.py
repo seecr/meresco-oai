6b7891d6e0a10ae4ca750fbed3dd22226f0947e9
Seecr Development Team <development@seecr.nl>
2015-07-13 11:24:17 +0200

    TS/TJ: asserted current behaviour of ResumptionToken, accept a lot of B$

--- test/resumptiontokentest.py
+++ test/resumptiontokentest.py
@@ -1,32 +1,33 @@
 ## begin license ##
-# 
+#
 # "Meresco Oai" are components to build Oai repositories, based on
-# "Meresco Core" and "Meresco Components". 
-# 
+# "Meresco Core" and "Meresco Components".
+#
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2010 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Delft University of Technology http://www.tudelft.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2012, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2012 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
-# 
+# Copyright (C) 2015 Koninklijke Bibliotheek (KB) http://www.kb.nl
+#
 # This file is part of "Meresco Oai"
-# 
+#
 # "Meresco Oai" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Oai" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Oai"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from meresco.oai.resumptiontoken import ResumptionToken, resumptionTokenFromString
@@ -38,7 +39,7 @@ class ResumptionTokenTest(SeecrTestCase):
         aTokenString = str(token)
         token2 = resumptionTokenFromString(aTokenString)
         self.assertEquals(token, token2)
-    
+
     def testResumptionToken(self):
         self.assertResumptionToken(ResumptionToken())
         resumptionToken = ResumptionToken(metadataPrefix='oai:dc', continueAfter='100', from_='2002-06-01T19:20:30Z', until='2002-06-01T19:20:39Z', set_='some:set:name')
@@ -50,4 +51,7 @@ class ResumptionTokenTest(SeecrTestCase):
         self.assertEquals('some:set:name', resumptionToken.set_)
         self.assertResumptionToken(ResumptionToken(set_=None))
 
-    
+    def testResumptionTokenHacked(self):
+        r = ResumptionToken.fromString('caap|f|m|u|s')
+        # complete nonsense is accepted (for now ????)
+        self.assertEquals('aap', r.continueAfter)
