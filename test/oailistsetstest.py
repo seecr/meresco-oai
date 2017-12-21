17e6237ffc39b8da95b8a4ca600ae8b5c2af874b
Jurjan-Paul Medema <jurjanpaul@seecr.nl>
2017-12-21 14:38:14 +0100

    JPM: set names XML escaped in ListSets response

---
+++ test/oailistsetstest.py
@@ -0,0 +1,45 @@
+## begin license ##
+#
+# "Meresco Oai" are components to build Oai repositories, based on
+# "Meresco Core" and "Meresco Components".
+#
+# Copyright (C) 2017 Seecr (Seek You Too B.V.) http://seecr.nl
+#
+# This file is part of "Meresco Oai"
+#
+# "Meresco Oai" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# "Meresco Oai" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Oai"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+
+from seecr.test import SeecrTestCase, CallTrace
+
+from weightless.core import Observable, be, asString
+
+from meresco.oai.oailistsets import OaiListSets
+
+
+class OaiListSetsTest(SeecrTestCase):
+    def testSetNameEscaped(self):
+        repository = CallTrace(returnValues=dict(requestUrl='http://example.org'))
+        observer = CallTrace(returnValues=dict(getAllSets=[('123_abc', '123 & abc')]), emptyGeneratorMethods=['oaiWatermark'])
+        top = be(
+            (Observable(),
+                (OaiListSets(repository),
+                    (observer,)
+                )
+            )
+        )
+        response = asString(top.all.listSets(arguments=dict(verb=['ListSets'])))
+        self.assertTrue('<ListSets><set><setSpec>123_abc</setSpec><setName>123 &amp; abc</setName></set></ListSets>' in response, response)
